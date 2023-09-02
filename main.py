e13f407abbf0bca4a5bb54f7ed47aa14367c4010
Foat Khisamov <foat.com>
2023-09-02 22:17:10 +1000

    Задание для обсуждения в группах

---
+++ main.py
@@ -0,0 +1,42 @@
+import sqlite3 as sl
+
+
+def create_table():
+#создаем пустую таблицу users со столбцами id, name, age если ее не было в БД
+    cur.execute("""CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        age INTEGER
+        )""")
+    con.commit()  # сохраняем изменения
+
+
+
+def add_into_empty():  #если таблица пустая то добавляется 2 записи
+    cur.execute("select * from users")
+    if not cur.fetchall(): #если вернулась пустота
+        cur.execute("INSERT INTO users (name,age) VALUES ('Иванов', 30)")
+        con.commit()#сохраняет изменение
+        cur.execute("INSERT INTO users (name,age) VALUES ('Петров', 33)")
+        con.commit()#сохраняет изменение
+
+def show_data(): # показываем все записи таблицы users
+    cur.execute("select * from users")
+    print(cur.fetchall())
+    # for row in cur.fetchall():
+    #     print(row)
+
+con = sl.connect("gb.db")   # соединяемся с БД , если ее нету, то создаем такую БД
+
+cur = con.cursor()        # создаем указатель , через него будем выполнять запросы
+# cur.execute("""
+# """)
+
+con.close()
+
+
+create_table()
+add_into_empty()
+show_data()
+
+
